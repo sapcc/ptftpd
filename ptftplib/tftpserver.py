efb2f91dcc839c1a027482f981bdd1ab7a575a56
Stefan Hipfel <stefan.hipfel@sap.com>
2020-11-11 17:04:37 +0100

    workaround for python bug which leads to empty packets

--- ptftplib/tftpserver.py
+++ ptftplib/tftpserver.py
@@ -143,6 +143,12 @@ class TFTPServerHandler(socketserver.DatagramRequestHandler):
             self.wfile.write(message)
             self.wfile.flush()
 
+    def finish(self):
+        """ Workaround for following bug: http://bugs.python.org/issue1767511 """
+        wdata = self.wfile.getvalue()
+        if wdata:
+            self.socket.sendto(wdata, self.client_address)        
+    
     def finish_state(self, peer_state):
         self.server.clients[self.client_address] = peer_state
         return peer_state.next()
